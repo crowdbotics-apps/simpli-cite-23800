3d22b2920bd145b2d4cc07f5729131e04b8c06da
Crowdbotics <team@crowdbotics.com>
2021-01-13 03:15:09 +0000

    Update models

--- backend/simpli_cite_23800/wsgi.py
+++ backend/simpli_cite_23800/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simpli_cite_23800.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simpli_cite_23800.settings")
 
 application = get_wsgi_application()
